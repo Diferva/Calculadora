982be53aaeca0d18edff4e20b9eea5479943ef5d
Diferva <119456017+Diferva@users.noreply.github.com>
2025-04-10 09:06:18 -0500

    First commit, uploading files

---
+++ OperacionesTrigonometricas.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class OperacionesTrigonometricas
+    {
+        public static double CalcularSeno(double anguloGrados)
+        {
+            double anguloRadianes = ConvertirGradosARadianes(anguloGrados);
+            return Math.Sin(anguloRadianes);
+        }
+
+        public static double CalcularCoseno(double anguloGrados)
+        {
+            double anguloRadianes = ConvertirGradosARadianes(anguloGrados);
+            return Math.Cos(anguloRadianes);
+        }
+
+        private static double ConvertirGradosARadianes(double grados)
+        {
+            return grados * (Math.PI / 180);
+        }
+    }
+
+
+}
